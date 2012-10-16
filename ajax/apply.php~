7d2474969b80689ba6216752e8c4deb08d633b0f
vbdetlevvb <fabi.arnold@ci4you.com>
2012-10-16 23:29:31 +0200

    Commit

---
+++ ajax/apply.php~
@@ -0,0 +1,4 @@
+
+<?
+shell_exec("echo windows123 | sudo -S /etc/init.d/freeradius start")
+?>
---
+++ config.php
@@ -0,0 +1,5 @@
+<?
+$config["ssh_host"] = "127.0.0.1";
+$config["ssh_user"] = "test";
+$config["ssh_password"] = "test123";
+?>
